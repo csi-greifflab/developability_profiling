1baf5d5e8150c7079fb403280d7807ebb8d8d72f
Jahn Zhong <friedsushi@protonmail.com>
2024-04-30 17:04:29 +0200

    Added immunopeptide region span.

--- scripts/developability/sequence_DPs/sequence_dp_calculations.R
+++ scripts/developability/sequence_DPs/sequence_dp_calculations.R
@@ -3,19 +3,22 @@
 library(Peptides)
 library(tidyverse)
 library(parallel)
-
+library(doParallel)
+library(foreach)
+library(stringr)
 
 #first load/read a dataframe that contains the sequences you are interested in
 
-developability_data = read_csv("~/data/dataframe.csv")
+developability_data = read_csv("~/data/dataframe.csv") #this data should include aaSeqAbChain column that contains the sequence of the variable region (fv) of the antibody.
 
-#this data should include aaSeqAbChain column that contains the sequence of the variable region (fv) of the antibody.
+# Specify number of cores for parallelization
+num_cores = 50
+cl <- makeCluster(num_cores)
 
 #charge----
 
 charge_intervals = as.character(c(1:14))
 
-cl <- makeCluster(50)
 
 for (i in 1:length(charge_intervals)){
   charge_intervals = as.character(c(1:14))
@@ -77,7 +80,7 @@ developability_data = developability_data %>%
 developability_data = developability_data %>%
   mutate(AbChain_percentextcoef = (AbChain_molextcoef*10)/AbChain_mw) %>%
   mutate(AbChain_cysbridges_percenextcoef = (AbChain_cysbridges_molextcoef*10)/AbChain_mw)
-        
+
 
 #indexes---- 
 get_instaindex = function(seq){
@@ -295,6 +298,106 @@ immuno_data = get_immunogenicity_measures(immuno_data) %>%
 
 developability_data = left_join(developability_data, immuno_data)
 
+# Get antibody region of immunogenic peptides
+
+get_regions = function (df, id) {
+  filtered = df %>% filter(rowid == id)
+  full_seq = pull(filtered, aaSeqAbChain)
+  fr1_seq = pull(filtered, aaSeqFR1)
+  fr2_seq = pull(filtered, aaSeqFR2)
+  fr3_seq = pull(filtered, aaSeqFR3)
+  fr4_seq = pull(filtered, aaSeqFR4)
+  cdr1_seq = pull(filtered, aaSeqCDR1)
+  cdr2_seq = pull(filtered, aaSeqCDR2)
+  cdr3_seq = pull(filtered, aaSeqCDR3)
+  
+  fr1_pos = str_locate(full_seq, fr1_seq)[,2]
+  fr2_pos = str_locate(full_seq, fr2_seq)[,2]
+  fr3_pos = str_locate(full_seq, fr3_seq)[,2]
+  fr4_pos = str_locate(full_seq, fr4_seq)[,2]
+  cdr1_pos = str_locate(full_seq, cdr1_seq)[,2]
+  cdr2_pos = str_locate(full_seq, cdr2_seq)[,2]
+  cdr3_pos = str_locate(full_seq, cdr3_seq)[,2]
+  positions = list(id, fr1_pos, cdr1_pos, fr2_pos, cdr2_pos, fr3_pos, cdr3_pos, fr4_pos)
+  return(positions)
+}
+
+get_peptide_position = function (region_pos) {
+  # Calculate peptide start and end regions and numbers of regions spanned by peptide
+  new_df = foreach (row = c(1:length(region_pos$rowid)), .combine = 'rbind', .init = tibble()) %dopar% {
+    end_not_found = TRUE
+    # Region 1 = FR1, Region 2 = CDR1, Region 3 = FR2, etc....
+    region = 1
+    while (end_not_found) {
+      # Increment region by one until it is matched (end_not_found == FALSE)
+      region = region + 1
+      # if peptide start region is smaller or equal to current region
+      if (region_pos[row,]$peptide_start <= region_pos[row,][region]) {
+        
+        #if peptide end region is smaller or equal to current region
+        if (region_pos[row,]$peptide_end <= region_pos[row,][region]) {
+          
+          #convert region number to region name and assign
+          peptide_start_region = colnames(region_pos)[region]
+          peptide_end_region = colnames(region_pos)[region]
+          peptide_regions_spanned = 1
+          end_not_found = FALSE
+          region = 2
+        }
+        else {
+          for (end_region in (region+1):8) {
+            if (region_pos[row,]$peptide_end <= region_pos[row,][end_region]) {
+              peptide_regions_spanned = end_region - region + 1
+              peptide_start_region = colnames(region_pos)[region]
+              peptide_end_region = colnames(region_pos)[end_region]
+              end_not_found = FALSE
+              region = 2
+              break
+            }
+          }
+        }
+      }
+    }
+    
+    frame = data.frame(as.double(region_pos[row, 1]),
+                       peptide_start_region,
+                       peptide_end_region,
+                       peptide_regions_spanned)
+    return(frame)
+  }
+  
+  # Rename output data frame
+  colnames(new_df) = c("rowid", 
+                       "peptide_start_region", 
+                       "peptide_end_region", 
+                       "peptide_regions_spanned")
+  
+  return(new_df)
+}
+
+get_reg_and_pept = function (df) {
+  temp_list = list(get_regions(df, df$rowid))
+  temp_frame = data.frame(temp_list) %>% as_tibble()
+  colnames(temp_frame) = c("rowid", "fr1", "cdr1", "fr2", "cdr2", "fr3", "cdr3", "fr4")
+  
+  peptide = pull(df, Peptide)
+  rowid = pull(df, rowid)
+  peptide_start = pull(df, AbChain_maximal_immuno_position)
+  peptide_end = peptide_start + nchar(peptide) - 1
+  peptide_df = tibble(data.frame(rowid, peptide_start, peptide_end))
+  
+  temp_frame = left_join(temp_frame, peptide_df)
+  
+  return(get_peptide_position(temp_frame))
+}
+
+immunogenicity_regions_span = developability_data %>%
+  filter(!is.na(AbChain_maximal_immuno_position)) %>% 
+  get_reg_and_pept() %>% 
+  rename(rowid = rowid,
+         AbChain_immunopeptide_regions_span = peptide_regions_spanned)
+
+developability_data = developability_data %>% left_join(immunogenicity_regions_span)
 
 #solubility----
 #(1) produce the input file as above (line 210) 
@@ -309,7 +412,6 @@ solubility_data = read_csv("solubility_result.csv") %>%
 
 
 developability_data = left_join(developability_data, solubility_data)
-
 
 
 
